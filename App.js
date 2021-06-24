ae161823978c8ababa87276e8256079c6b3dc2d3
jblavisse <jean-baptiste@jblavisse.fr>
2021-06-24 10:42:02 +0200

    List of posts with map

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => {
+      console.log(res.data);
+      setPosts(res.data);
+    })
+  }, [])
+
+  const postsJSX = posts.map(post => {
+    return <Text>{post.title}</Text>
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {postsJSX}
     </View>
   );
 }
