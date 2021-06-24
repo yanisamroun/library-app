c690da012aaf32b248a2f636ee05d6288d17473a
jblavisse <jean-baptiste@jblavisse.fr>
2021-06-24 14:01:11 +0200

    Set stack navigator with home screen

--- App.js
+++ App.js
@@ -1,85 +1,17 @@
-import React, {useState, useEffect} from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import axios from 'axios';
-import { ListItem, SocialIcon, Switch } from 'react-native-elements';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(res => {
-      console.log(res.data);
-      setPosts(res.data);
-    })
-  }, [])
-
-  // const postsJSX = posts.map(post => {
-  //   return <Text>{post.title}</Text>
-  // })
-
-
-  // function truc() {
-  //   return (<View>
-  //     <Text>fokfoofk</Text>
-  //   </View>);
-  // }
-
-  // const truc = ()  => {
-  // return (
-  // <View>
-  //   <Text>fokfoofk</Text>
-  // </View>)
-  // };
-
-  // const truc = ()  => (
-  //   <View>
-  //     <Text>fokfoofk</Text>
-  //   </View>)
-
   return (
-    <View style={styles.container}>
-      <SocialIcon type='twitter' />
-      {/* {postJSX} */}
-      {/* 
-          - data -> Un tableau d'éléments qu'on veut rendre 
-          (très souvent c'est un état)
-          - renderItem -> fonction de rendu 
-          (qu'est-ce que vous voulez pour chaque élément)
-          item c'est un nom qu'on ne peut pas changer,
-          item est imposé
-
-          - keyExtractor , c'est la même que le key=
-          qu'on mettait pour enlever le warning de la console
-
-          <ScrollView>
-           {postsJSX}
-          </ScrollView> mais encore plus optimisé, car
-          ça charge dynamiquement
-      */}
-      <FlatList
-        data={posts}
-
-        renderItem={({item})=> (
-          <ListItem bottomDivider>
-          <ListItem.Content>
-            <ListItem.Title>{item.title}</ListItem.Title>
-            <Switch value={false} />
-          </ListItem.Content>
-          <ListItem.Chevron />
-          </ListItem>)
-        }
-
-        keyExtractor={item => item.id.toString()}
-      />
-      
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { ListItem, SocialIcon, Switch } from 'react-native-elements';
+
+export default function HomeScreen() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => {
+      console.log(res.data);
+      setPosts(res.data);
+    })
+  }, [])
+
+  return (
+    <View style={styles.container}>
+    <SocialIcon type='twitter' />
+    <FlatList
+      data={posts}
+
+      renderItem={({item})=> (
+        <ListItem bottomDivider>
+        <ListItem.Content>
+          <ListItem.Title>{item.title}</ListItem.Title>
+          <Switch value={false} />
+        </ListItem.Content>
+        <ListItem.Chevron />
+        </ListItem>)
+      }
+
+      keyExtractor={item => item.id.toString()}
+    />
+    
+  </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+});
